d4ae75d580f6c62fdbe003f04dbc382131c0c129
vaysse emma <emma.vaysse.bourgeois@gmail.com>
2024-10-07 11:17:16 +0200

    correction

--- VendorMachine.ts
+++ VendorMachine.ts
@@ -3,22 +3,28 @@ class VendorMachine {
   private snacksQty: number;
   private money: number;
 
-  public buySnacks() {
+  public buySnacks(): void {
     this.isOn = true;
     this.money = +2;
     this.snacksQty = -1;
   }
 
-  public reset() {
+  public reset(): void {
     this.isOn = false;
-    this.snacksQty = +50;
+    this.snacksQty = 50;
     this.money = 0;
     this.isOn = true;
   }
 
-  public shootWithFoot() {
+  public shootWithFoot(): void {
     this.snacksQty = -5;
     this.money = -20;
     this.isOn = false;
   }
 }
+
+const vendorMachine = new VendorMachine();
+
+vendorMachine.reset();
+vendorMachine.shootWithFoot();
+vendorMachine.buySnacks;
